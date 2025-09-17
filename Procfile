de401d86fad5318860d2470df4c8e560861327af
Your Name <you@example.com>
2025-09-17 15:03:47 +0530

    Inital

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
-web: gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
-gunicorn doctor_appointmen.wsgi:application
-gunicorn config.wsgi:application
 gunicorn doctor_appointmen.wsgi:application
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doctor_appointmen.settings")
 application = get_wsgi_application()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doctor_appointmen.settings")
