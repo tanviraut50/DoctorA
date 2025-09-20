cf50c0ed280f122f3cfc19bf82a2be2b2268c09d
Your Name <you@example.com>
2025-09-20 16:38:20 +0530

    Intial commit

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,13 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
